a792d8887d220ebaad833212a6790cf96c321703
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-07-02 22:27:09 +0300

    added migrations

---
+++ backend/database/migrations/2025_07_02_191516_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('slug')->unique(); // optional: for clean URLs
+            $table->text('description');
+
+            $table->decimal('price', 8, 2);
+            $table->decimal('discount_price', 8, 2)->nullable();
+            $table->boolean('on_sale')->default(false);
+
+            $table->integer('stock_quantity');
+            $table->string('image'); 
+
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
